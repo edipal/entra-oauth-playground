1ca55c91fc47827ac5b0df8ba48cdd3512853907
EDUARD PAL <eduard.pal@ic-consult.com>
2025-10-15 21:10:12 +0200

    remove test error pages

--- app/[locale]/boom/page.tsx
+++
@@ -1,6 +0,0 @@
-// Server-side error trigger to test error boundary and logging
-export const dynamic = 'force-dynamic';
-
-export default function BoomPage() {
-  throw new Error('Intentional server error from /[locale]/boom');
-}
